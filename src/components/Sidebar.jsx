178118aa216151092264992abdadf872f860ca9f
Rahul Pranami <rahulpranami101@gmail.com>
2022-11-14 17:40:59 +0530

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,7 +28,11 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <img
+          src={logo}
+          alt="logo"
+          className="w-full object-contain bg-[#252240] rounded-xl"
+        />
         <NavLinks />
       </div>
 
